8458c7b0522cb4127f769bcdb898983f14dc27d8
Ad9m <im4damgr7@gmail.com>
2023-03-08 13:19:02 +0100

    first react

---
+++ first_react/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <h3>* Learn React </h3>
+      <h3>* Climb Mt.Everest </h3>
+      <h3>* Run a marathon </h3>
+      <h3>* Feed the dogs</h3>
+      
+    </div>
+  );
+}
+
+export default App;
