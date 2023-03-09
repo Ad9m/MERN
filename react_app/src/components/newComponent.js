062b4800e221bee2da09634aa6213eb5cae07d35
Ad9m <im4damgr7@gmail.com>
2023-03-09 13:38:32 +0100

    inversion

--- react_app/src/components/newComponent.js
+++ react_app/src/components/newComponent.js
@@ -1,30 +1,13 @@
-import React, { Component } from 'react';
-import classes from './MyNewComponent.module.css';
-
-class PersonComponent extends Component{
-    constructor(props){
-    super(props);
-    this.state ={
-        age:this.props.age
-    }
-    }
-    changeAge =() =>{
-        this.setState({age:this.state.age +1});
-    }
-    render(){
-
-        return(
-            <div className={classes.card}>
-                <div className={classes.content}>
-                    <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                    <p>Age: {this.state.age}</p>
-                    <p>Hair Color: {this.props.hairColor}</p>
-                    <button onClick={this.changeAge}>Birthday Buttton for {this.props.firstname} {this.props.lastname}</button>
-                </div>
-            </div>
-        );
-
-    }
+import React from 'react';
+const PersonCard = props => {
+    return(
+        <div>
+            <h1>{ props.lastName }, { props.firstName }</h1>
+            <p>Age: { props.age }</p>
+            <p>Hair Color: { props.hairColor }</p>
+        </div>
+    );
 }
+export default PersonCard;
 
-export default PersonComponent;
+
