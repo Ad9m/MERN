d1a67eac71a653d7ed8ca0760f83b809379d3b68
Ad9m <im4damgr7@gmail.com>
2023-03-12 11:55:58 +0100

    box generator

---
+++ box_generator/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Boxform from './components/boxform/boxform';
+
+function App() {
+  return (
+    <div className="container">
+    <Boxform />
+  </div>
+  );
+}
+
+export default App;
---
+++ box_generator/src/components/boxform/Boxform.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import "./boxform.css";
+
+export default class Boxform {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: '',
+            errors: {}
+        }
+    }
+    handleChange = (e) => {
+        let newColor = e.target.value;
+        this.setState({
+            color: newColor
+        })
+    }
+    addColor = (e) => {
+        e.preventDefault();
+        if(!this.validateForm()){
+            return;
+        }
+        this.props.handleSubmit(this.state.color)
+        this.setState({color: ''});
+    }
+    validateForm = (e) => {
+        const errors = {}
+        // Checks if the color is entered
+        if(this.state.color.length === 0){
+            errors.color = 'You must enter a color.'
+        }
+        // Counts the number of keys in the object and if the length is grater than zero it throws and error
+        if(Object.keys(errors).length > 0){
+            this.setState({errors});
+            return false
+        }
+        return true
+    }
+    render() {
+        // Creates an error element when called
+        const errorMessage =(message) => (<span className="BoxForm__error">
+            {message}
+          </span>)
+        return (
+            <div className='BoxForm'>
+                <form className='BoxForm__form' onSubmit={this.addColor} action="">
+                    <label htmlFor="color" className='BoxForm__label'>Choose color</label>
+                    <input type="text" name="color" id="color" onKeyDown={() =>delete this.state.errors.color} className="BoxForm__input" onChange={this.handleChange} value={this.state.color} />
+                    {this.state.errors.color?errorMessage(this.state.errors.color):''}
+                    <button type="submit" className='BoxForm__button'  >Add box</button>
+                </form>
+            </div>
+        )
+    }
+}
